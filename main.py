04a76cd1a17311cfb4ff5ae724bfc95fd306d162
Feraclin <feraclin@gmail.com>
2023-05-15 18:09:41 +0300

    clear trash

--- main.py
+++
@@ -1,4 +0,0 @@
-with open('input.txt', 'r', encoding='utf-8') as file:
-    string: str = file.readline()
-with open('output.txt', 'w', encoding='utf-8') as out:
-    file.write(str(sum([int(i) for i in string.strip().split()])))
