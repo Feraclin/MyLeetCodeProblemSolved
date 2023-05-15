6413f1fbd1114f01303c5210b40efd315b4373c7
Feraclin <feraclin@gmail.com>
2023-05-15 17:33:33 +0300

    some task

--- Go LeetCode/Populating Next Right Pointers in Each Node.go
+++ Go LeetCode/Populating Next Right Pointers in Each Node.go
@@ -4,13 +4,20 @@ func connect(root *Node) *Node {
 	if root == nil {
 		return nil
 	}
+
+	// Установка связи между левым и правым дочерними узлами текущего узла
 	if root.Left != nil {
 		root.Left.Next = root.Right
 	}
-	if root.Next != nil {
+
+	// Установка связи между правым дочерним узлом текущего узла и левым дочерним узлом его соседнего узла
+	if root.Right != nil && root.Next != nil {
 		root.Right.Next = root.Next.Left
 	}
+
+	// Рекурсивный вызов функции connect для левого и правого дочерних узлов текущего узла
 	connect(root.Left)
 	connect(root.Right)
+
 	return root
 }
